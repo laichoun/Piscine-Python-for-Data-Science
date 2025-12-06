a122a38890c562eb083fa8f72973d4907f9e99ea
laichoun <aichouniasma@gmail.com>
2025-12-06 18:52:19 +0100

    ex00

---
+++ Python_1/ex00/give_bmi.py
@@ -0,0 +1,90 @@
+
+def give_bmi(height: list[int | float],
+             weight: list[int | float]) -> list[int | float]:
+    checkArgs(height, weight)
+
+    bmiList = []
+    for h, w in zip(height, weight):
+        bmiList.append(w/(h * h))
+    return (bmiList)
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    res = []
+    for item in bmi:
+        if (item > limit):
+            res.append(True)
+        else:
+            res.append(False)
+    return (res)
+
+
+def checkArgs(height, weight) -> int:
+    if (len(height) != len(weight)):
+        raise ValueError("Length are not the same.")
+    for x in height:
+        if (isinstance(x, int) or isinstance(x, float)):
+            continue
+        else:
+            raise ValueError("Error, not a list of int or float.")
+    for y in weight:
+        if (isinstance(x, int) or isinstance(x, float)):
+            continue
+        else:
+            raise ValueError("Error, not a list of int or float.")
+
+
+def main():
+    try:
+        height = [2.71, 1.15]
+        weight = [165.3, 38.4]
+
+        bmi = give_bmi(height, weight)
+        print(bmi, type(bmi))
+        print(apply_limit(bmi, 26))
+    except ValueError as e:
+        print(e)
+
+    try:
+        height = [2.71, 1.15, "Hello"]
+        weight = [165.3, 38.4]
+
+        bmi = give_bmi(height, weight)
+        print(bmi, type(bmi))
+        print(apply_limit(bmi, 26))
+    except ValueError as e:
+        print(e)
+
+    try:
+        height = [2.71, 1.15, "Hello"]
+        weight = [165.3, 38.4]
+
+        bmi = give_bmi(height, weight)
+        print(bmi, type(bmi))
+        print(apply_limit(bmi, 26))
+    except ValueError as e:
+        print(e)
+
+    try:
+        height = [2.71, 1.15, True]
+        weight = [165.3, 38.4]
+
+        bmi = give_bmi(height, weight)
+        print(bmi, type(bmi))
+        print(apply_limit(bmi, 26))
+    except ValueError as e:
+        print(e)
+
+    try:
+        height = [2.71, 1.15]
+        weight = [165.3, 3]
+
+        bmi = give_bmi(height, weight)
+        print(bmi, type(bmi))
+        print(apply_limit(bmi, 26))
+    except ValueError as e:
+        print(e)
+
+
+if (__name__ == "__main__"):
+    main()
