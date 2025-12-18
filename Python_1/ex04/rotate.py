766a8c36684a7020c28b5e739c4e5d9e7e288b72
laichoun <aichouniasma@gmail.com>
2025-12-18 11:34:25 +0100

    ex04 done

--- Python_1/ex04/rotate.py
+++ Python_1/ex04/rotate.py
@@ -4,34 +4,34 @@ import numpy as np
 
 
 def main():
-	arr = ft_load("animal.jpeg")
-	if (arr is None):
-		return (1)
-	ySlice = slice(100, 500)
-	xSlice = slice(450, 850)
+    arr = ft_load("animal.jpeg")
+    if (arr is None):
+        return (1)
+    ySlice = slice(100, 500)
+    xSlice = slice(450, 850)
     # 1 canal.
-	cSlice = slice(0, 1)
+    cSlice = slice(0, 1)
     # intensity of the coulor chosen
-	zoomed = arr[ySlice, xSlice, cSlice]
-	zoomed_gray = zoomed[:, :, 0]
-	print(f"The shape of image is: {zoomed.shape} or {zoomed_gray.shape}")
-	print(zoomed)
-	
-	print(zoomed_gray)
-	# print(trans)
-	
-	# transpose : i ligne et j colonne
+    zoomed = arr[ySlice, xSlice, cSlice]
+    zoomed_gray = zoomed[:, :, 0]
+    print(f"The shape of image is: {zoomed.shape} or {zoomed_gray.shape}")
+    print(zoomed)
 
-	#tranArr = np.array()
-	for line in zoomed_gray:
-		for elmt in line:
-			print(elmt)
+    tab = []
 
-	plt.imshow(zoomed_gray, cmap="gray")
-	plt.show()
+    for i, col in enumerate(zoomed_gray[0]):
+        ligne = []
+        for line in zoomed_gray:
+            ligne.append(line[i])
+        tab.append(ligne)
+
+    tranArr = np.array(tab)
+    print("New shape after Transpose:", tranArr.shape)
+    print(tranArr)
+
+    plt.imshow(tranArr, cmap="gray")
+    plt.show()
 
 
-
-
-if (__name__== "__main__"):
-	main()
+if (__name__ == "__main__"):
+    main()
