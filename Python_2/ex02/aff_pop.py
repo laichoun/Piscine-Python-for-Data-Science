f1f8735ddb8c48e68a331c60088120b4d2ac6297
laichoun <aichouniasma@gmail.com>
2026-01-12 17:34:22 +0100

    add legend

--- Python_2/ex02/aff_pop.py
+++ Python_2/ex02/aff_pop.py
@@ -11,19 +11,24 @@ def aff_pop():
     dataLux.index = dataLux.index.astype(int)
     dataBel.index = dataBel.index.astype(int)
 
+    print(dataLux.values)
+
     print("1800 - 2050", dataLux.loc[1800:2050])
     dfLuxY = dataLux.loc[1800:2050]
     dfBelY = dataBel.loc[1800:2050]
+
+    dfLuxY = dfLuxY.str.replace("M", "").astype(float)
+    dfBelY = dfBelY.str.replace("M", "").astype(float)
     print("dfLuxY", dfLuxY)
     print("dfBelY", dfBelY)
 
     plt.xlabel("Years")
     plt.ylabel("Population")
     plt.title("Population Projections")
-    plt.plot(dfLuxY)
-    plt.plot(dfBelY)
-
-    plt.yticks()
+    plt.plot(dfLuxY, label="Lux")
+    
+    plt.plot(dfBelY, label="Belgium")
+    plt.legend()
 
     plt.show()
 
