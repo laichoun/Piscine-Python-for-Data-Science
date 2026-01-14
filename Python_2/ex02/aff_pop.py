237eb32f8b0dd5224a2aeab3ee4ff750e63ef474
laichoun <aichouniasma@gmail.com>
2026-01-14 17:46:10 +0100

    exo02 modified because rules with k and M not optimized

--- Python_2/ex02/aff_pop.py
+++ Python_2/ex02/aff_pop.py
@@ -18,26 +18,68 @@ def aff_pop():
     dfLuxY = dataLux.loc[1800:2050]
     dfBelY = dataBel.loc[1800:2050]
 
-    dfLuxY = dfLuxY.str.replace("M", "").astype(float)
-    dfBelY = dfBelY.str.replace("M", "").astype(float)
-    print("dfLuxY", dfLuxY)
+    # for i, item in enumerate(dfLuxY):
+    #     print(f"Index {i}: {item}")
+    #     print("test", dfLuxY.iloc[i])
+    #     if ("k" in dfLuxY.iloc[i]):
+    #         print("Ok trouvé k")
+    #         dfLuxY.iloc[i].replace("k", "").astype(float)
+    #         dfLuxY.iloc[i] = dfLuxY.iloc[i] * 1000
+    #     elif ("M" in dfLuxY.iloc[i]):
+    #         print("Ok trouvé M")
+    #         dfLuxY.iloc[i].replace("M", "").astype(float)
+    #         dfLuxY.iloc[i] = dfLuxY.iloc[i] * 1000000
+
+
+    # dfLuxY = dfLuxY.where(
+    #     ~dfLuxY.str.contains("k"),
+    #     dfLuxY.str.replace("k", "", regex=False).astype(float) * 1_000  
+    # )
+
+    dfLuxY = dfLuxY.where(
+        ~dfLuxY.str.contains("M"),
+        dfLuxY.str.replace("M", "", regex=False).astype(float) * 1_000_000
+    )
+
+    dfBelY = dfBelY.where(
+        ~dfBelY.str.contains("M"),
+        dfBelY.str.replace("M", "", regex=False).astype(float) * 1_000_000
+    )
+
+#     dfLuxY = (
+#     dfLuxY.str.lower()
+#     .str.replace("k", "e3", regex=False)
+#     .str.replace("m", "e6", regex=False)
+#     .astype(float)
+# )
+    
+#     dfBelY = (
+#     dfBelY.str.lower()
+#     .str.replace("k", "e3", regex=False)
+#     .str.replace("m", "e6", regex=False)
+#     .astype(float)
+# )
+
+
+    # dfBelY = dfBelY.str.replace("M", "").astype(float)
+    print("dfLuxY N", dfLuxY)
     print("dfBelY", dfBelY)
 
     plt.xlabel("Years")
     plt.ylabel("Population")
     plt.title("Population Projections")
-    plt.plot(dfLuxY, label="Lux")
-    
-    plt.plot(dfBelY, label="Belgium")
-    plt.legend()
-
-    #get current axe
+    # get current axe
     ax = plt.gca()
     ax.yaxis.set_major_locator(ticker.MaxNLocator(4))
-    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f"{int(x)}M"))
-    
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x,
+                                                      pos: f"{int(x)}M"))
     ticks = list(range(1800, 2050, 40))
     plt.xticks(ticks)
+
+    plt.plot(dfLuxY, label="Lux", color='green')
+    plt.plot(dfBelY, label="Belgium", color='blue')
+    plt.legend(loc='lower right')
+
     plt.show()
 
 
