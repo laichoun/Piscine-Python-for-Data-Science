3ac41dce91628f77376e51947011eee5c30a1fc7
laichoun <aichouniasma@gmail.com>
2026-01-15 16:28:26 +0100

    exo3 done

---
+++ Python_2/ex03/load_csv.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def load(path: str):
+    try:
+        # print(pd.__version__)
+        df = pd.read_csv(path)
+        print(f"Loading dataset of dimensions {df.shape}")
+        df = df.set_index("country")
+        return (df)
+
+    except FileNotFoundError:
+        print(f"FileNotFoundError:[Errno 2] No such file or directory: {path}")
+        return (None)
+
+
+def main():
+    print(load("population_total.csv"))
+
+
+if (__name__ == "__main__"):
+    main()
---
+++ Python_2/ex03/projection_life.py
@@ -0,0 +1,52 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.ticker as ticker
+# import seaborn as sns
+
+
+def projection_life():
+    dfIncome = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+    dfExpLife = load("life_expectancy_years.csv")
+    
+    dfIncome.columns = dfIncome.columns.astype(int)
+    dfExpLife.columns = dfExpLife.columns.astype(int)
+
+    dfIncome1900 = dfIncome[2027]
+    dfExpLife1900 = dfExpLife[2027]
+
+    # dfIncome1900 = (
+    #     dfIncome1900
+    #     .str.replace("k", "e3", regex=False)
+    #     .str.replace("M", "e6", regex=False)
+    #     .astype(float)
+    # )
+
+    print(dfIncome1900, dfExpLife1900)
+
+    newDf = pd.concat([dfIncome1900, dfExpLife1900], axis=1)
+    newDf.columns=["GNP", "Expectancy life"]
+    newDf.dropna(inplace=True)
+    subset = newDf[(newDf["GNP"] >= 7000) & (newDf["GNP"] <= 10000)]
+    print("SUB",subset)
+    print(newDf.to_string())
+
+    print("corr", newDf.corr())
+    print("duplicated?", newDf.duplicated().to_string())
+
+    newDf.plot(kind = 'scatter', x = 'GNP', y = 'Expectancy life')
+    ax = plt.gca()
+    ax.set_xlim(100, 10_000)
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(4))
+    plt.title("1900")
+    plt.xlabel("Gross domestic product")
+    plt.ylabel("Life Expectancy")
+    plt.show()
+    
+
+def main():
+    projection_life()
+
+
+if (__name__ == "__main__"):
+    main()
