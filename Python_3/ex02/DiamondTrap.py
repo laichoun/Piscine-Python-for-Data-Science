382d2ac0fd806ec6841ac12bb95d3be3498a1e16
laichoun <aichouniasma@gmail.com>
2026-02-09 13:11:23 +0100

    exo3 done + norminette ex02

--- Python_3/ex02/DiamondTrap.py
+++ Python_3/ex02/DiamondTrap.py
@@ -2,41 +2,43 @@
 
 from S1E7 import Baratheon, Lannister
 
+
 class King(Baratheon, Lannister):
-	"""
+    """
 Docstring for King
 Child of Baratheon and Lannister.
 Diamond inheritance : python uses C3 linearization to decide the order.
 This order is called MRO (method resolution order)
 The order will always be King -> Baratheon -> Lannister -> Character -> Object
-	"""
-	def __init__(self, first_name, is_alive = True):
-		super().__init__(first_name, is_alive = True)
-	
-	def set_eyes(self, newEyes):
-		self.eyes = newEyes
-	
-	def set_hairs(self, newHairs):
-		self.hairs = newHairs
+    """
+    def __init__(self, first_name, is_alive=True):
+        super().__init__(first_name, is_alive=True)
 
-	def get_eyes(self):
-		return (self.eyes)
-	
-	def get_hairs(self):
-		return (self.hairs)
+    def set_eyes(self, newEyes):
+        self.eyes = newEyes
+
+    def set_hairs(self, newHairs):
+        self.hairs = newHairs
+
+    def get_eyes(self):
+        return (self.eyes)
+
+    def get_hairs(self):
+        return (self.hairs)
 
 
 def main():
-	Joffrey = King("Joffrey")
-	print(Joffrey.__dict__)
-	Joffrey.set_eyes("blue")
-	# print(Joffrey.__dict__)
-	Joffrey.set_hairs("light")
-	print(Joffrey.get_eyes())
-	print(Joffrey.get_hairs())
-	print(Joffrey.__dict__)
+    Joffrey = King("Joffrey")
+    print(Joffrey.__dict__)
+    Joffrey.set_eyes("blue")
+    # print(Joffrey.__dict__)
+    Joffrey.set_hairs("light")
+    print(Joffrey.get_eyes())
+    print(Joffrey.get_hairs())
+    print(Joffrey.__dict__)
 
-	print(King.mro())
+    print(King.mro())
+
 
 if (__name__ == "__main__"):
-	main()
+    main()
--- Python_3/ex03/ft_calculator.py
+++ Python_3/ex03/ft_calculator.py
@@ -1,23 +1,50 @@
 #!/usr/bin/python3
 
-class calculator :
+class calculator:
+    """
+Do the overloading operators + - * /
+    """
+    def __init__(self, val):
+        self.value = val
 
-	def __add__(self, objet) -> None:
-		return (self + objet)
+    def __add__(self, object) -> None:
+        myList = [x + object for x in self.value]
+        return calculator(myList)
 
-	def __mul__(self, objet) -> None:
-		pass
+    def __mul__(self, object) -> None:
+        myList = [x * object for x in self.value]
+        return calculator(myList)
 
-	def __sub__(self, objet) -> None:
-		pass
+    def __sub__(self, object) -> None:
+        myList = [x - object for x in self.value]
+        return calculator(myList)
 
-	def __truediv__(self, objet) -> None:
-		pass
+    def __truediv__(self, object) -> None:
+        if (object == 0):
+            raise ValueError("Not possible to divide with 0")
+        else:
+            myList = [x / object for x in self.value]
+            return calculator(myList)
+
+    def __repr__(self):
+        return (f"{self.value}")
+
 
 def main():
-	v1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
-	# v1+ 5
+    v1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+    print(v1 + 5)
+    print("---")
+    v2 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+    v2 * 5
+    print("---")
+    v3 = calculator([10.0, 15.0, 20.0])
+    v3 - 5
+    try:
+        v3 / 5
+        v3/0
+    except ValueError as e:
+        print(e)
 
 
 if (__name__ == "__main__"):
-	main()
+    main()
